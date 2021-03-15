e810f3d4eb4ac31030c309fb31ab4b63a0583432
Salah-Eddine HARNOUFI <salah-eddine.harnoufi@ensq.uvsq.fr>
2021-03-15 19:26:10 +0100

    base de code incendie.py

---
+++ incendie.py
@@ -0,0 +1,39 @@
+# Commentaire avant de coder:
+# 1. mettre les bouts de code dans les parties correspondantes..
+# .. Modules importés, Constantes, Variable globale, Fonction principale, Placement des widgets
+# 2. Tout le code les algorithmes, les if, else, les conditions, etc... doivent être mis.. 
+# ..dans les fonctions
+# 3. nommer les constantes en majuscule, mettez une docstring au debut de chaque fonction ..
+# et utiliser de bon noms, exemple creer_balle, LONGUEUR, etc...
+
+
+######################################
+##### Projet : incendie
+##### Participants:
+# Nour cherif
+# Salah-Eddine Harnoufi
+# Karim Maurel
+# Léon
+# issouf
+# sissokho abdoulah
+##### Groupe:
+# td: 5
+# Cm: 1
+######################################
+
+#####################################
+# Modules importés: tkinter
+import tkinter as tk
+
+#####################################
+# Constantes
+
+#####################################
+# Variables globales (si besoin)
+
+#####################################
+# Fonctions principales
+
+
+#####################################
+# Placement des widgets
